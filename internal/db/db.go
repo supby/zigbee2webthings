715c0eaf25f2939d0bc20e8628ac1180b6658ad6
Andre Suzanovich <supius@gmail.com>
2022-09-30 21:00:32 +0200

    add device db impl

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,10 +1,42 @@
 package db
 
-import "context"
+import (
+	"context"
 
-type DB interface {
+	"github.com/cockroachdb/pebble"
+)
+
+type DeviceDB interface {
 	GetDevices(ctx context.Context) (error, []Device)
 	AddDevice(ctx context.Context, device Device) error
 	DeleteDevice(ctx context.Context, ieeeAddress uint64) error
 	UpdateDevice(ctx context.Context, device Device) error
+}
+
+func NewDeviceDB(filename string) (DeviceDB, error) {
+	db, err := pebble.Open("demo", &pebble.Options{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &deviceDB{}, nil
+}
+
+type deviceDB struct {
+}
+
+func (d *deviceDB) GetDevices(ctx context.Context) (error, []Device) {
+	panic("implement me!")
+}
+
+func (d *deviceDB) AddDevice(ctx context.Context, device Device) error {
+	panic("implement me!")
+}
+
+func (d *deviceDB) DeleteDevice(ctx context.Context, ieeeAddress uint64) error {
+	panic("implement me!")
+}
+
+func (d *deviceDB) UpdateDevice(ctx context.Context, device Device) error {
+	panic("implement me!")
 }
