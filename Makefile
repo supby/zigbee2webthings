81bdf305ec234df21d4fe0f3d616c8fd30cba7d6
Andre Suzanovich <supius@gmail.com>
2022-10-02 18:56:31 +0200

    implement db with pebble

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ default:
 
 rpi_build:
 	env GOOS=linux GOARCH=arm GOARM=6 go build -o ./bin/zigbee2webthings ./cmd/zigbee2webthings/main.go
+
+build_all:
+	go build ./...
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,42 +1,82 @@
 package db
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"encoding/gob"
 
 	"github.com/cockroachdb/pebble"
 )
 
 type DeviceDB interface {
-	GetDevices(ctx context.Context) (error, []Device)
-	AddDevice(ctx context.Context, device Device) error
+	GetDevices(ctx context.Context) ([]Device, error)
+	SaveDevice(ctx context.Context, device Device) error
 	DeleteDevice(ctx context.Context, ieeeAddress uint64) error
-	UpdateDevice(ctx context.Context, device Device) error
 }
 
 func NewDeviceDB(filename string) (DeviceDB, error) {
-	db, err := pebble.Open("demo", &pebble.Options{})
+	db, err := pebble.Open(filename, &pebble.Options{})
 	if err != nil {
 		return nil, err
 	}
 
-	return &deviceDB{}, nil
+	return &deviceDB{
+		db: db,
+	}, nil
 }
 
 type deviceDB struct {
+	db *pebble.DB
 }
 
-func (d *deviceDB) GetDevices(ctx context.Context) (error, []Device) {
-	panic("implement me!")
+func (d *deviceDB) GetDevices(ctx context.Context) ([]Device, error) {
+	iter := d.db.NewIter(&pebble.IterOptions{})
+
+	var ret []Device
+	for iter.Next() {
+		d := Device{
+			IEEEAddress: binary.LittleEndian.Uint64(iter.Key()),
+		}
+
+		dec := gob.NewDecoder(bytes.NewReader(iter.Value()))
+		err := dec.Decode(&d)
+		if err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, d)
+	}
+
+	return ret, nil
 }
 
-func (d *deviceDB) AddDevice(ctx context.Context, device Device) error {
-	panic("implement me!")
+func (d *deviceDB) SaveDevice(ctx context.Context, device Device) error {
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, uint64(device.IEEEAddress))
+
+	buf := bytes.Buffer{}
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(device)
+	if err != nil {
+		return err
+	}
+
+	if err := d.db.Set(key, buf.Bytes(), pebble.Sync); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (d *deviceDB) DeleteDevice(ctx context.Context, ieeeAddress uint64) error {
-	panic("implement me!")
-}
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, uint64(ieeeAddress))
 
-func (d *deviceDB) UpdateDevice(ctx context.Context, device Device) error {
-	panic("implement me!")
+	err := d.db.Delete(key, &pebble.WriteOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
